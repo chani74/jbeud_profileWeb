9ff7afcc8522e18ca0baa84d8967a14b7ebe0002
이은찬 <chani74@gmail.com>
2025-01-10 10:50:28 +0900

    INIT

---
+++ src/test/java/com/chani74/profile/ProfileWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chani74.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
